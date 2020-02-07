a3506cd387466c771fba01d69990b80608f666d3
jose galindo <jose.galindo@mitec.com.mx>
2020-02-07 17:27:53 -0600

    Initial Commit

---
+++ LiverpoolTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiverpoolTest
+//
+//  Created by Jose Cruz Galindo Martinez on 07/02/20.
+//  Copyright © 2020 JGM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
